ded8b917b282338914a711e4d9baaf1fc6a82191
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2026-04-09 20:36:06 +0700

    feat: pnpm-install

--- pnpm-install.ps1
+++ pnpm-install.ps1
@@ -20,110 +20,42 @@
 #
 ################################################################################
 
-# Application info
-$GITHUB_OWNER="pnpm"
-$GITHUB_REPO="pnpm"
-$PROGRAM_ID="pnpm"
-$PROGRAM_EXEC="pnpm.exe"
-
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 Set-PSDebug -Trace 2
 
-# Process arguments
-$LATEST_VERSION = ""
+# Application info
+$GITHUB_OWNER = "pnpm"
+$GITHUB_REPO = "pnpm"
+$PROGRAM_ID = "pnpm"
+$PROGRAM_EXEC = "pnpm.exe"
+
+# Shared library
+. "${PSScriptRoot}\shared\install-common.ps1"
+
+# Prepare environment
+$INSTALL_VERSION = ""
 if (${args}.Count -ge 1) {
-	$LATEST_VERSION = ${args}[0].ToString()
+	$INSTALL_VERSION = ${args}[0].ToString()
 }
-
-# Determine version to install
-if ( "${LATEST_VERSION}" -eq "" ) {
-	$LATEST_VERSION = (Invoke-WebRequest "https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/releases/latest" | ConvertFrom-Json).tag_name
-}
-$LATEST_VERSION = ${LATEST_VERSION}.TrimStart("v")
-
-# Installation environment
-$IS_ADMIN = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-$ARCH = ${env:PROCESSOR_ARCHITECTURE}
-if (${IS_ADMIN}) {
-	$INSTALL_ROOT = [IO.Path]::Combine("${env:PROGRAMFILES}", "${PROGRAM_ID}")
-	$TEMP_ROOT = "${env:TEMP}"
-} else {
-	$INSTALL_ROOT = [IO.Path]::Combine("${env:USERPROFILE}", ".local", "share", "${PROGRAM_ID}")
-	$TEMP_ROOT = "${env:TEMP}"
-}
-if (${env:CLIINST_USR_LOCAL_SHARE} -ne $null) {
-	$INSTALL_ROOT = [IO.Path]::Combine("${env:CLIINST_USR_LOCAL_SHARE}", "${PROGRAM_ID}")
-}
-if (${env:CLIINST_TEMP} -ne $null) {
-	$TEMP_ROOT = "${env:CLIINST_TEMP}"
-}
-$INSTALL_TARGET = [IO.Path]::Combine("${INSTALL_ROOT}", "${PROGRAM_ID}-v${LATEST_VERSION}")
-$ACTIVE_TARGET = [IO.Path]::Combine("${INSTALL_ROOT}", "active-release")
-
-if (!(Test-Path "${INSTALL_ROOT}" -PathType Container)) {
-	New-Item -ItemType Directory -Path "${INSTALL_ROOT}" -Force
-}
-if (!(Test-Path "${TEMP_ROOT}" -PathType Container)) {
-	New-Item -ItemType Directory -Path "${TEMP_ROOT}" -Force
-}
+$INSTALL_VERSION = Get-InstallVersionFromGithub -Owner "${GITHUB_OWNER}" -Repo "${GITHUB_REPO}" -FallbackVersion "${INSTALL_VERSION}"
+$InstallEnv = Initialize-InstallEnv -ProgramId "${PROGRAM_ID}" -Version "${INSTALL_VERSION}"
 
 # Download and install binaries
-if (!(Test-Path "${INSTALL_TARGET}" -PathType Container)) {
-	$BIN_URI_AMD64 = "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/download/v${LATEST_VERSION}/pnpm-win-x64.exe"
-	$BIN_URI_ARM64 = "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/download/v${LATEST_VERSION}/pnpm-win-arm64.exe"
-	$BIN_TMP_FILE = [IO.Path]::Combine("${TEMP_ROOT}", "${PROGRAM_ID}-v${LATEST_VERSION}.exe")
-
-	$ProgressPreference = "SilentlyContinue"
-	if ("${ARCH}" -eq "AMD64") {
-		Invoke-WebRequest -Uri "${BIN_URI_AMD64}" -OutFile "${BIN_TMP_FILE}"
-	}
-	elseif ("${ARCH}" -eq "ARM64") {
-		Invoke-WebRequest -Uri "${BIN_URI_ARM64}" -OutFile "${BIN_TMP_FILE}"
-	}
-	else {
-		Write-Output "Unsupported architecture: ${ARCH}"
-		exit 1
-	}
-
-	$TEMP_TARGET = [IO.Path]::Combine("${TEMP_ROOT}", "${PROGRAM_ID}-v${LATEST_VERSION}")
-	New-Item -ItemType Directory -Path "${TEMP_TARGET}"
-	Move-Item "${BIN_TMP_FILE}" $([IO.Path]::Combine("${TEMP_TARGET}", "${PROGRAM_EXEC}"))
-	$TEMP_TARGET_FINAL = $([IO.Path]::Combine("${TEMP_TARGET}"))
-	Move-Item "${TEMP_TARGET_FINAL}" "${INSTALL_TARGET}"
-	if (Test-Path "${TEMP_TARGET}") {
-		Remove-Item "${TEMP_TARGET}" -Recurse -Force
-	}
-	$ProgressPreference = "Continue"
+if (!(Test-Path "$($InstallEnv.InstallTarget)" -PathType Container)) {
+	$BIN_URI_AMD64 = "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/download/v${INSTALL_VERSION}/pnpm-win-x64.exe"
+	$BIN_URI_ARM64 = "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/download/v${INSTALL_VERSION}/pnpm-win-arm64.exe"
+	$BIN_TMP_FILE = "$($InstallEnv.TempTarget).exe"
+	Download-UriPerArch -Amd64Uri "${BIN_URI_AMD64}" -Arm64Uri "${BIN_URI_ARM64}" -OutputPath "${BIN_TMP_FILE}"
+	New-Directory2 "$($InstallEnv.TempTarget)"
+	Move-Item2 -From "${BIN_TMP_FILE}" -To ([IO.Path]::Combine($($InstallEnv.TempTarget), ${PROGRAM_EXEC}))
+	Move-Item2 -From "$([IO.Path]::Combine($($InstallEnv.TempTarget)))" -To "$($InstallEnv.InstallTarget)"
+	Remove-Item2 "$($InstallEnv.TempTarget)"
 }
 
-# Set active release
-if (Test-Path "${ACTIVE_TARGET}") {
-	Remove-Item "${ACTIVE_TARGET}" -Recurse -Force
-}
-New-Item -ItemType Junction -Target "${INSTALL_TARGET}" -Path "${ACTIVE_TARGET}"
-
-# Update PATH for quick access
-$ACTIVE_TARGET_BIN = [IO.Path]::Combine("${ACTIVE_TARGET}")
-$CLIINST_PATH = ${env:CLIINST_PATH}
-if (${CLIINST_PATH} -eq $null) {
-	$CLIINST_PATH = "${ACTIVE_TARGET_BIN}"
-}
-else {
-	$CLIINST_PATHS = ${CLIINST_PATH}.Split(";")
-	if (${CLIINST_PATHS}.IndexOf("${ACTIVE_TARGET_BIN}") -eq -1) {
-		$CLIINST_PATH = "${CLIINST_PATH};${ACTIVE_TARGET_BIN}"
-		$CLIINST_PATHS = ${CLIINST_PATH}.Split(";")
-	}
-	$CLIINST_PATHS = ${CLIINST_PATHS} | Sort-Object -Unique
-	$CLIINST_PATH = $CLIINST_PATHS -Join ";"
-}
-if (${IS_ADMIN}) {
-	[Environment]::SetEnvironmentVariable("CLIINST_PATH", "${CLIINST_PATH}", "Machine")
-}
-else {
-	[Environment]::SetEnvironmentVariable("CLIINST_PATH", "${CLIINST_PATH}", "User")
-}
+# Finalize install
+Link-Item2 -From "$($InstallEnv.InstallTarget)" -To "$($InstallEnv.ActiveTarget)" -Overwrite
+Update-CliinstPath -BinPath "$($InstallEnv.ActiveTarget)" -IsSystemWide $($InstallEnv.IsAdmin)
 Write-Output "Installation completed"
 Write-Output "New terminal session must be started before installing other applications using this script to avoid lost ENVAR issue"
 
