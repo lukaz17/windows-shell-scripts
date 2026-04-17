febc634b1815859ac30ae1d0b4952248a16778a7
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2026-04-17 09:57:57 +0700

    feat: jellyfin-install

--- jellyfin-install.ps1
+++ jellyfin-install.ps1
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #
 #  jellyfin-install
@@ -22,124 +20,43 @@
 #
 ################################################################################
 
-# Application info
-$GITHUB_OWNER="jellyfin"
-$GITHUB_REPO="jellyfin"
-$PROGRAM_ID="Jellyfin"
-$PROGRAM_NAME="Jellyfin"
-$PROGRAM_EXEC="jellyfin.exe"
-
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 Set-PSDebug -Trace 2
 
-# Process arguments
-$LATEST_VERSION = ""
+# Application info
+$GITHUB_OWNER = "jellyfin"
+$GITHUB_REPO = "jellyfin"
+$PROGRAM_ID = "Jellyfin"
+$PROGRAM_EXEC = "jellyfin.exe"
+
+# Shared library
+. "${PSScriptRoot}\shared\install-common.ps1"
+
+# Prepare environment
+$INSTALL_VERSION = ""
 if (${args}.Count -ge 1) {
-	$LATEST_VERSION = ${args}[0].ToString()
+	$INSTALL_VERSION = ${args}[0].ToString()
 }
-
-# Determine version to install
-if ( "${LATEST_VERSION}" -eq "" ) {
-	$LATEST_VERSION = (Invoke-WebRequest "https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/releases/latest" | ConvertFrom-Json).tag_name
-}
-$LATEST_VERSION = ${LATEST_VERSION}.TrimStart("v")
-
-# Installation environment
-$IS_ADMIN = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-$ARCH = ${env:PROCESSOR_ARCHITECTURE}
-if (${IS_ADMIN}) {
-	$INSTALL_ROOT = [IO.Path]::Combine("${env:PROGRAMFILES}", "${PROGRAM_ID}")
-	$TEMP_ROOT = "${env:TEMP}"
-} else {
-	$INSTALL_ROOT = [IO.Path]::Combine("${env:USERPROFILE}", ".local", "share", "${PROGRAM_ID}")
-	$TEMP_ROOT = "${env:TEMP}"
-}
-if (${env:CLIINST_USR_LOCAL_SHARE} -ne $null) {
-	$INSTALL_ROOT = [IO.Path]::Combine("${env:CLIINST_USR_LOCAL_SHARE}", "${PROGRAM_ID}")
-}
-if (${env:CLIINST_TEMP} -ne $null) {
-	$TEMP_ROOT = "${env:CLIINST_TEMP}"
-}
-$INSTALL_TARGET = [IO.Path]::Combine("${INSTALL_ROOT}", "${PROGRAM_ID}-v${LATEST_VERSION}")
-$ACTIVE_TARGET = [IO.Path]::Combine("${INSTALL_ROOT}", "active-release")
-
-if (!(Test-Path "${INSTALL_ROOT}" -PathType Container)) {
-	New-Item -ItemType Directory -Path "${INSTALL_ROOT}" -Force
-}
-if (!(Test-Path "${TEMP_ROOT}" -PathType Container)) {
-	New-Item -ItemType Directory -Path "${TEMP_ROOT}" -Force
-}
+$INSTALL_VERSION = Get-InstallVersionFromGithub -Owner "${GITHUB_OWNER}" -Repo "${GITHUB_REPO}" -FallbackVersion "${INSTALL_VERSION}"
+$InstallEnv = Initialize-InstallEnv -ProgramId "${PROGRAM_ID}" -Version "${INSTALL_VERSION}"
 
 # Download and install binaries
-if (!(Test-Path "${INSTALL_TARGET}" -PathType Container)) {
-	$BIN_ARCH_URI_AMD64 = "https://repo.jellyfin.org/files/server/windows/stable/v${LATEST_VERSION}/amd64/jellyfin_${LATEST_VERSION}-amd64.zip"
-	$BIN_ARCH_URI_ARM64 = "https://repo.jellyfin.org/files/server/windows/stable/v${LATEST_VERSION}/arm64/jellyfin_${LATEST_VERSION}-arm64.zip"
-	$BIN_ARCH_TMP_FILE = [IO.Path]::Combine(${TEMP_ROOT}, "${PROGRAM_ID}-v${LATEST_VERSION}.zip")
-
-	$ProgressPreference = "SilentlyContinue"
-	if ("${ARCH}" -eq "AMD64") {
-		Invoke-WebRequest -Uri "${BIN_ARCH_URI_AMD64}" -OutFile "${BIN_ARCH_TMP_FILE}"
-	}
-	elseif ("${ARCH}" -eq "ARM64") {
-		Invoke-WebRequest -Uri "${BIN_ARCH_URI_ARM64}" -OutFile "${BIN_ARCH_TMP_FILE}"
-	}
-	else {
-		Write-Output "Unsupported architecture: ${ARCH}"
-		exit 1
-	}
-
-	$TEMP_TARGET = [IO.Path]::Combine("${TEMP_ROOT}", "${PROGRAM_ID}-v${LATEST_VERSION}")
-	New-Item -ItemType Directory -Path "${TEMP_TARGET}"
-	if ("${ARCH}" -eq "AMD64") {
-		$7ZCLI = [IO.Path]::Combine("${PSScriptRoot}", "7z-amd64", "7z.exe")
-		& "${7ZCLI}" x "${BIN_ARCH_TMP_FILE}" -o"${TEMP_TARGET}"
-	}
-	elseif ("${ARCH}" -eq "ARM64") {
-		$7ZCLI = [IO.Path]::Combine("${PSScriptRoot}", "7z-arm64", "7z.exe")
-		& "${7ZCLI}" x "${BIN_ARCH_TMP_FILE}" -o"${TEMP_TARGET}"
-	}
-	else {
-		Write-Output "Unsupported architecture: ${ARCH}"
-		exit 1
-	}
-	Remove-Item "${BIN_ARCH_TMP_FILE}" -Force
-	$TEMP_TARGET_FINAL = $([IO.Path]::Combine("${TEMP_TARGET}", "jellyfin"))
-	Move-Item "${TEMP_TARGET_FINAL}" "${INSTALL_TARGET}"
-	if (Test-Path "${TEMP_TARGET}") {
-		Remove-Item "${TEMP_TARGET}" -Recurse -Force
-	}
-	$ProgressPreference = "Continue"
+if (!(Test-Path "$($InstallEnv.InstallTarget)" -PathType Container)) {
+	$BIN_ARCH_URI_AMD64 = "https://repo.jellyfin.org/files/server/windows/stable/v${INSTALL_VERSION}/amd64/jellyfin_${INSTALL_VERSION}-amd64.zip"
+	$BIN_ARCH_URI_ARM64 = "https://repo.jellyfin.org/files/server/windows/stable/v${INSTALL_VERSION}/arm64/jellyfin_${INSTALL_VERSION}-arm64.zip"
+	$BIN_ARCH_TMP_FILE = "$($InstallEnv.TempTarget).zip"
+	Download-UriPerArch -Amd64Uri "${BIN_ARCH_URI_AMD64}" -Arm64Uri "${BIN_ARCH_URI_ARM64}" -OutputPath "${BIN_ARCH_TMP_FILE}"
+	New-Directory2 "$($InstallEnv.TempTarget)"
+	Extract-Archive -ArchivePath "${BIN_ARCH_TMP_FILE}" -DestinationPath "$($InstallEnv.TempTarget)" -ScriptRoot "${PSScriptRoot}"
+	Remove-Item2 "${BIN_ARCH_TMP_FILE}"
+	$TEMP_TARGET_FINAL = $([IO.Path]::Combine("$($InstallEnv.TempTarget)", "jellyfin"))
+	Move-Item2 -From "${TEMP_TARGET_FINAL}" -To "$($InstallEnv.InstallTarget)"
+	Remove-Item2 "$($InstallEnv.TempTarget)"
 }
 
-# Set active release
-if (Test-Path "${ACTIVE_TARGET}") {
-	Remove-Item "${ACTIVE_TARGET}" -Recurse -Force
-}
-New-Item -ItemType Junction -Target "${INSTALL_TARGET}" -Path "${ACTIVE_TARGET}"
-
-# Update PATH for quick access
-$ACTIVE_TARGET_BIN = [IO.Path]::Combine("${ACTIVE_TARGET}")
-$CLIINST_PATH = ${env:CLIINST_PATH}
-if (${CLIINST_PATH} -eq $null) {
-	$CLIINST_PATH = "${ACTIVE_TARGET_BIN}"
-}
-else {
-	$CLIINST_PATHS = ${CLIINST_PATH}.Split(";")
-	if (${CLIINST_PATHS}.IndexOf("${ACTIVE_TARGET_BIN}") -eq -1) {
-		$CLIINST_PATH = "${CLIINST_PATH};${ACTIVE_TARGET_BIN}"
-		$CLIINST_PATHS = ${CLIINST_PATH}.Split(";")
-	}
-	$CLIINST_PATHS = ${CLIINST_PATHS} | Sort-Object -Unique
-	$CLIINST_PATH = $CLIINST_PATHS -Join ";"
-}
-if (${IS_ADMIN}) {
-	[Environment]::SetEnvironmentVariable("CLIINST_PATH", "${CLIINST_PATH}", "Machine")
-}
-else {
-	[Environment]::SetEnvironmentVariable("CLIINST_PATH", "${CLIINST_PATH}", "User")
-}
-Write-Output "Installation completed"
-Write-Output "New terminal session must be started before installing other applications using this script to avoid lost ENVAR issue"
+# Finalize install
+Link-Item2 -From "$($InstallEnv.InstallTarget)" -To "$($InstallEnv.ActiveTarget)" -Overwrite
+Update-CliinstPath -BinPath "$($InstallEnv.ActiveTarget)" -IsSystemWide $($InstallEnv.IsAdmin)
 
 Set-PSDebug -Trace 0
