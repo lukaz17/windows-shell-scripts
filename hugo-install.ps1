8d8fd3a26588ae1199f3a9be694b2fcc1696175d
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2026-04-17 10:07:54 +0700

    feat: hugo-install

---
+++ hugo-install.ps1
@@ -0,0 +1,62 @@
+################################################################################
+#
+#  hugo-install
+#
+#  Install/Update Hugo for current user. This also works as a version manager.
+#  If version is not specified, the latest version will be installed.
+#
+#  Homepage: https://gohugo.io/
+#
+#  Usage:
+#    hugo-install [<version>]
+#
+#  Example:
+#    hugo-install
+#    hugo-install 0.170.1
+#    hugo-install v0.170.1
+#
+#  MIT License.
+#  Copyright (C) 2025 Nguyen Nhat Tung.
+#
+################################################################################
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+Set-PSDebug -Trace 2
+
+# Application info
+$GITHUB_OWNER = "gohugoio"
+$GITHUB_REPO = "hugo"
+$PROGRAM_ID = "Hugo"
+$PROGRAM_EXEC = "hugo.exe"
+
+# Shared library
+. "${PSScriptRoot}\shared\install-common.ps1"
+
+# Prepare environment
+$INSTALL_VERSION = ""
+if (${args}.Count -ge 1) {
+	$INSTALL_VERSION = ${args}[0].ToString()
+}
+$INSTALL_VERSION = Get-InstallVersionFromGithub -Owner "${GITHUB_OWNER}" -Repo "${GITHUB_REPO}" -FallbackVersion "${INSTALL_VERSION}"
+$InstallEnv = Initialize-InstallEnv -ProgramId "${PROGRAM_ID}" -Version "${INSTALL_VERSION}"
+
+# Download and install binaries
+if (!(Test-Path "$($InstallEnv.InstallTarget)" -PathType Container)) {
+	$BIN_ARCH_URI_AMD64 = "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/download/v${INSTALL_VERSION}/hugo_extended_withdeploy_${INSTALL_VERSION}_windows-amd64.zip"
+	$BIN_ARCH_URI_ARM64 = ""
+	$BIN_ARCH_TMP_FILE = "$($InstallEnv.TempTarget).zip"
+	Download-UriPerArch -Amd64Uri "${BIN_ARCH_URI_AMD64}" -Arm64Uri "${BIN_ARCH_URI_ARM64}" -OutputPath "${BIN_ARCH_TMP_FILE}"
+	New-Directory2 "$($InstallEnv.TempTarget)"
+	Extract-Archive -ArchivePath "${BIN_ARCH_TMP_FILE}" -DestinationPath "$($InstallEnv.TempTarget)" -ScriptRoot "${PSScriptRoot}"
+	Remove-Item2 "${BIN_ARCH_TMP_FILE}"
+	$TEMP_TARGET_FINAL = $([IO.Path]::Combine("$($InstallEnv.TempTarget)"))
+	Move-Item2 -From "${TEMP_TARGET_FINAL}" -To "$($InstallEnv.InstallTarget)"
+	Remove-Item2 "$($InstallEnv.TempTarget)"
+}
+
+# Finalize install
+Link-Item2 -From "$($InstallEnv.InstallTarget)" -To "$($InstallEnv.ActiveTarget)" -Overwrite
+Update-CliinstPath -BinPath "$($InstallEnv.ActiveTarget)" -IsSystemWide $($InstallEnv.IsAdmin)
+
+Set-PSDebug -Trace 0
