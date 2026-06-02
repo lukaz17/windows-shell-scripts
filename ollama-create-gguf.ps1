d997f613c729a8f742fef3f51e55fb040f3bafdf
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2026-06-02 11:03:21 +0700

    feat: ollama-create-gguf for with multiple source files

--- ollama-create-gguf.ps1
+++ ollama-create-gguf.ps1
@@ -3,19 +3,17 @@
 #  ollama-create-gguf
 #
 #  Create a custom model for Ollama from an local gguf file.
-#  If version is not specified, the latest version will be installed.
 #  The script won't handle input validations and delegate it to Ollama.
 #
 #  Usage:
-#    ollama-create-gguf -Model <model:tag> -Src <path> [-ContextSize <KB>] [-KVCache <fp16/f16/q8_0/q4_0>] [-Verify <0/1>]
+#    ollama-create-gguf -Model <model:tag> -Src <path> -Src2 [<path>] [-ContextSize <KB>] [-KVCache <fp16/f16/q8_0/q4_0>] [-Verify <0/1>]
 #
 #  Example:
-#    ollama-create-gguf llama-3.3-nemotron-super:49b-it-q4_K_M .\Llama-3_3-Nemotron-Super-49B-v1_5-Q4_K_M.gguf
-#    ollama-create-gguf llama-3.3-nemotron-super:49b-it-q4_K_M .\Llama-3_3-Nemotron-Super-49B-v1_5-Q4_K_M.gguf 128
-#    ollama-create-gguf llama-3.3-nemotron-super:49b-it-q4_K_M .\Llama-3_3-Nemotron-Super-49B-v1_5-Q4_K_M.gguf 128 q8_0
-#    ollama-create-gguf llama-3.3-nemotron-super:49b-it-q4_K_M .\Llama-3_3-Nemotron-Super-49B-v1_5-Q4_K_M.gguf 128 q8_0 1
-#    ollama-create-gguf llama-3.3-nemotron-super:49b-it-q4_K_M .\Llama-3_3-Nemotron-Super-49B-v1_5-Q4_K_M.gguf -ContextSize 128 -KVCache q8_0 -Verify 1
-#    ollama-create-gguf -Model llama-3.3-nemotron-super:49b-it-q4_K_M -Src .\Llama-3_3-Nemotron-Super-49B-v1_5-Q4_K_M.gguf -ContextSize 128 -KVCache q8_0 -Verify 1
+#    ollama-create-gguf qwen3.6:27b-q8_0 .\Qwen3.6-27B-Q8_0.gguf
+#    ollama-create-gguf qwen3.6:27b-q8_0 .\Qwen3.6-27B-Q8_0.gguf -Verify 1
+#    ollama-create-gguf qwen3.6:27b-q8_0 .\Qwen3.6-27B-Q8_0.gguf -ContextSize 256 -Verify 1
+#    ollama-create-gguf qwen3.6-vl:27b-q8_0 .\Qwen3.6-27B-Q8_0.gguf .\mmproj-F16.gguf -ContextSize 256 -Verify 1
+#    ollama-create-gguf -Model qwen3.6-vl:27b-q8_0 -Src .\Qwen3.6-27B-Q8_0.gguf -Src2 .\mmproj-F16.gguf -ContextSize 256 -Verify 1
 #
 #  MIT License.
 #  Copyright (C) 2025 Nguyen Nhat Tung.
@@ -28,8 +26,10 @@ param(
     [string]$Model,
 
     [Parameter(Mandatory = $true)]
-    [ValidateScript({Test-Path $_ -PathType Leaf})]
-    [string]$Src,
+    [string[]]$Src,
+
+    [Parameter(Mandatory = $false)]
+    [string[]]$Src2,
 
     [Parameter(Mandatory = $false)]
     [int]$ContextSize,
@@ -43,11 +43,13 @@ param(
 
 $NEW_MODEL = "${Model}"
 $MODEL_GGUF = "${Src}"
-$KV_CACHE = "${KVCache}"
-$LOAD_N_VERIFY = $(${Verify} -ne 0)
+$MODEL_GGUF_2 = "${Src2}"
 
 $mfFile = [IO.Path]::Combine(${env:TEMP}, "ollama_$([System.IO.Path]::GetRandomFileName()).Modelfile")
 $mfContent = "FROM ${MODEL_GGUF}"
+if ($PSBoundParameters.ContainsKey('Src2')) {
+	$mfContent += "`nFROM ${MODEL_GGUF_2}"
+}
 if ($PSBoundParameters.ContainsKey('ContextSize')) {
 	$mfContent += "`nPARAMETER num_ctx $(${ContextSize} * 1024)"
 }
@@ -62,47 +64,15 @@ try {
 	if ($LASTEXITCODE -ne 0) {
 		throw "ollama create failed with exit code $LASTEXITCODE"
 	}
-
-	if (${LOAD_N_VERIFY}) {
-		Write-Host "> Load model into memory."
-		$loadBody = @{
-			model      = $Model
-			messages   = @()
-			keep_alive = 60
-		} | ConvertTo-Json
-
-		try {
-			Invoke-RestMethod -Uri "http://127.0.0.1:11434/api/chat"-Method Post -Body ${loadBody} -ContentType "application/json" | Out-Null
-		} catch {
-			throw "Failed to load model into memory: $_"
-		}
-
-		Write-Host "> Query model details."
-		try {
-			$psResponse = Invoke-RestMethod `
-				-Uri    "http://127.0.0.1:11434/api/ps" `
-				-Method Get
-		} catch {
-			throw "Failed to query /api/ps: $_"
-		}
-
-		$first = $psResponse.models | Select-Object -First 1
-		if ($null -eq $first) {
-			throw "Model not found in /api/ps"
-		}
-
-		Write-Host "Name               : $($first.name)"
-		Write-Host "Parameter Size     : $($first.details.parameter_size)"
-		Write-Host "Quantization Level : $($first.details.quantization_level)"
-		Write-Host "Family             : $($first.details.family)"
-		Write-Host "Context Size       : $($first.context_length)"
-		Write-Host "Memory             : $($first.size)"
-		Write-Host "> Success."
-	} else {
-		Write-Host "> Success."
-		Start-Sleep -Seconds 1
-	}
 } finally {
 	Remove-Item -Path ${mfFile} -ErrorAction SilentlyContinue
 }
-Write-Host ""
+
+$params = @{
+	Model = $Model
+}
+if ($PSBoundParameters.ContainsKey('ContextSize')) { $params['ContextSize'] = $ContextSize }
+if ($PSBoundParameters.ContainsKey('KVCache'))     { $params['KVCache']     = $KVCache }
+if ($PSBoundParameters.ContainsKey('Verify'))      { $params['Verify']      = $Verify }
+
+& "$PSScriptRoot\ollama-create.ps1" @params
