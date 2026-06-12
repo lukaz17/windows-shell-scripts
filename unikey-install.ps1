a04a236078cf94c4a52a2999d16dda3c87dcea39
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2026-06-12 15:44:22 +0700

    feat: unikey-install

---
+++ unikey-install.ps1
@@ -0,0 +1,61 @@
+################################################################################
+#
+#  unikey-install
+#
+#  Install/Update Unikey for current user. This also works as a version manager.
+#
+#  Homepage: https://www.unikey.org/
+#
+#  Usage:
+#    unikey-install <version>
+#
+#  Example:
+#    unikey-install 4.5-rc3
+#    unikey-install v4.5-rc3
+#
+#  MIT License.
+#  Copyright (C) 2025 Nguyen Nhat Tung.
+#
+################################################################################
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+# Application info
+$PROGRAM_ID = "Unikey"
+$PROGRAM_NAME = "Unikey"
+$PROGRAM_EXEC = "UnikeyNT.exe"
+
+# Shared libraries
+. "${PSScriptRoot}\shared\install-common.ps1"
+. "${PSScriptRoot}\shared\install-source.ps1"
+
+# Prepare environment
+$INSTALL_VERSION = ""
+if (${args}.Count -ge 1) {
+	$INSTALL_VERSION = ${args}[0].ToString()
+}
+$INSTALL_VERSION = Normalize-InstallVersion "${INSTALL_VERSION}"
+$InstallEnv = Initialize-InstallEnv -ProgramId "${PROGRAM_ID}" -Version "${INSTALL_VERSION}" -IsApplication
+
+# Download and install binaries
+if (!(Test-Path "$($InstallEnv.InstallTarget)" -PathType Container)) {
+	$BIN_ARCH_URI_AMD64 = Get-Amd64Uri -ProgramId "${PROGRAM_ID}" -InstallVersion "${INSTALL_VERSION}"
+	$BIN_ARCH_URI_ARM64 = Get-Arm64Uri -ProgramId "${PROGRAM_ID}" -InstallVersion "${INSTALL_VERSION}"
+	$BIN_ARCH_TMP_FILE = "$($InstallEnv.TempTarget).zip"
+	Download-UriPerArch -Amd64Uri "${BIN_ARCH_URI_AMD64}" -Arm64Uri "${BIN_ARCH_URI_ARM64}" -OutputPath "${BIN_ARCH_TMP_FILE}"
+	New-Directory2 "$($InstallEnv.TempTarget)"
+	Extract-Archive -ArchivePath "${BIN_ARCH_TMP_FILE}" -DestinationPath "$($InstallEnv.TempTarget)" -ScriptRoot "${PSScriptRoot}"
+	Remove-Item2 "${BIN_ARCH_TMP_FILE}"
+	$TEMP_TARGET_FINAL = $([IO.Path]::Combine("$($InstallEnv.TempTarget)"))
+	Move-Item2 -From "${TEMP_TARGET_FINAL}" -To "$($InstallEnv.InstallTarget)"
+	Remove-Item2 "$($InstallEnv.TempTarget)"
+}
+
+# Finalize install
+Link-Item2 -From "$($InstallEnv.InstallTarget)" -To "$($InstallEnv.ActiveTarget)" -Overwrite
+$TARGET_EXE=[IO.Path]::Combine($($InstallEnv.ActiveTarget), ${PROGRAM_EXEC})
+New-AppShortcut -ProgramName "${PROGRAM_NAME}" -TargetExe "${TARGET_EXE}" -WorkingDir "$($InstallEnv.ActiveTarget)" -Destination "$($InstallEnv.DesktopRoot)"
+New-AppShortcut -ProgramName "${PROGRAM_NAME}" -TargetExe "${TARGET_EXE}" -WorkingDir "$($InstallEnv.ActiveTarget)" -Destination "$($InstallEnv.StartMenuRoot)"
+
+Write-Host "> Success."
