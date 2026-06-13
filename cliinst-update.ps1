7e0978fd24f52ea3b05f5619932052b0e234e1f8
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2026-06-13 15:15:34 +0700

    feat: cliinst-update

---
+++ cliinst-update.ps1
@@ -0,0 +1,33 @@
+################################################################################
+#
+#  cliinst-update
+#
+#  Update CliInst to latest version.
+#
+#  Usage:
+#    cliinst-update
+#
+#  MIT License.
+#  Copyright (C) 2025 Nguyen Nhat Tung.
+#
+################################################################################
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+$gitDir = Join-Path $PSScriptRoot ".git"
+
+if (Test-Path -LiteralPath "${gitDir}" -PathType Container) {
+	if (Get-Command git -ErrorAction SilentlyContinue) {
+		try {
+			git -C "${PSScriptRoot}" pull --rebase
+			Write-Host "> Success."
+		} catch {
+			Write-Warning "Failed to update: $_"
+		}
+	} else {
+		Write-Warning "Git command not found."
+	}
+} else {
+	Write-Warning "This is not a git repository."
+}
