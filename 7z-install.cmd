5507932bc56953041e09ba19224b9d138472cc21
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2026-07-15 14:57:36 +0700

    feat: 7z-install

--- 7z-install.cmd
+++ 7z-install.cmd
@@ -1,2 +1,1 @@
-echo %~dp0
 powershell -f "%~dp0%~n0.ps1" %*
--- 7z-install.ps1
+++ 7z-install.ps1
@@ -22,45 +22,43 @@
 
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
-Set-PSDebug -Trace 2
 
 # Application info
-$GITHUB_OWNER="ip7z"
-$GITHUB_REPO="7zip"
 $PROGRAM_ID="7-Zip"
 $PROGRAM_NAME="7-Zip"
 $PROGRAM_EXEC="7zFM.exe"
 
 # Shared library
 . "${PSScriptRoot}\shared\install-common.ps1"
+. "${PSScriptRoot}\shared\install-source.ps1"
 
 # Prepare environment
 $INSTALL_VERSION = ""
 if (${args}.Count -ge 1) {
 	$INSTALL_VERSION = ${args}[0].ToString()
 }
-$INSTALL_VERSION = Normalize-InstallVersion "${INSTALL_VERSION}"
+$InstallSrc = Get-InstallSource "${PROGRAM_ID}"
+$INSTALL_VERSION = Get-InstallVersionFromGithub -Owner "$($InstallSrc.GithubOwner)" -Repo "$($InstallSrc.GithubRepo)" -FallbackVersion "${INSTALL_VERSION}"
 $InstallEnv = Initialize-InstallEnv -ProgramId "${PROGRAM_ID}" -Version "${INSTALL_VERSION}" -IsApplication
 
 # Download and install binaries
 if (!(Test-Path "$($InstallEnv.InstallTarget)" -PathType Container)) {
-	$BIN_ARCH_URI_AMD64 = "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/download/${INSTALL_VERSION}/7z$(${INSTALL_VERSION}.Replace('.',''))-x64.exe"
-	$BIN_ARCH_URI_ARM64 = "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/download/${INSTALL_VERSION}/7z$(${INSTALL_VERSION}.Replace('.',''))-arm64.exe"
+	$BIN_ARCH_URI_AMD64 = Get-Amd64Uri -ProgramId "${PROGRAM_ID}" -InstallVersion "${INSTALL_VERSION}"
+	$BIN_ARCH_URI_ARM64 = Get-Arm64Uri -ProgramId "${PROGRAM_ID}" -InstallVersion "${INSTALL_VERSION}"
 	$BIN_ARCH_TMP_FILE = "$($InstallEnv.TempTarget).exe"
 	Download-UriPerArch -Amd64Uri "${BIN_ARCH_URI_AMD64}" -Arm64Uri "${BIN_ARCH_URI_ARM64}" -OutputPath "${BIN_ARCH_TMP_FILE}"
 	New-Directory2 "$($InstallEnv.TempTarget)"
 	Extract-Archive -ArchivePath "${BIN_ARCH_TMP_FILE}" -DestinationPath "$($InstallEnv.TempTarget)" -ScriptRoot "${PSScriptRoot}"
 	Remove-Item2 "${BIN_ARCH_TMP_FILE}"
 	$TEMP_TARGET_FINAL = $([IO.Path]::Combine("$($InstallEnv.TempTarget)"))
-	Move-Item2 -From "$($InstallEnv.TempTarget)" -To "$($InstallEnv.InstallTarget)"
+	Move-Item2 -From "${TEMP_TARGET_FINAL}" -To "$($InstallEnv.InstallTarget)"
 	Remove-Item2 "$($InstallEnv.TempTarget)"
 }
 
 # Finalize install
-Copy-Item2 -From "$($InstallEnv.InstallTarget)" -To "$($InstallEnv.ActiveTarget)" -Overwrite
+Link-Item2 -From "$($InstallEnv.InstallTarget)" -To "$($InstallEnv.ActiveTarget)" -Overwrite
 $TARGET_EXE=[IO.Path]::Combine($($InstallEnv.ActiveTarget), ${PROGRAM_EXEC})
 New-AppShortcut -ProgramName "${PROGRAM_NAME}" -TargetExe "${TARGET_EXE}" -WorkingDir "$($InstallEnv.ActiveTarget)" -Destination "$($InstallEnv.DesktopRoot)"
 New-AppShortcut -ProgramName "${PROGRAM_NAME}" -TargetExe "${TARGET_EXE}" -WorkingDir "$($InstallEnv.ActiveTarget)" -Destination "$($InstallEnv.StartMenuRoot)"
 Update-CliinstPath -BinPath "$($InstallEnv.ActiveTarget)" -IsSystemWide $($InstallEnv.IsAdmin)
-
-Set-PSDebug -Trace 0
+Finalize-Install
